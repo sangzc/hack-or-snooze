6e3da2123885b3ede3daf87691e1a17e4ef132b1
Sandy <sangzicao@gmail.com>
2019-02-07 14:28:26 -0800

    initial commit

---
+++ api-classes.js
@@ -0,0 +1,168 @@
+const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+
+/**
+ * This class maintains the list of individual Story instances
+ *  It also has some methods for fetching, adding, and removing stories
+ */
+class StoryList {
+  constructor(stories) {
+    this.stories = stories;
+  }
+  /**
+   * This method is designed to be called to generate a new StoryList.
+   *  It calls the API, builds an array of Story instances, makes a single StoryList
+   * instance out of that, and then returns the StoryList instance
+   */
+  static async getStories() {
+    // query the /stories endpoint (no auth required)
+    const response = await $.getJSON(`${BASE_URL}/stories`);
+    // turn the plain old story objects from the API into instances of the Story class
+    const stories = response.stories.map(story => new Story(story));
+    // build an instance of our own class using the new array of stories
+    const storyList = new StoryList(stories)
+    return storyList;
+  }
+  /**
+     * Method to make a POST request to /stories and add the new story to the list
+     The function should accept the current instance of User who will post the story
+     It should also accept an object which with a title, author, and url
+     */
+  
+  async addStory(user, newStory) {
+    // TODO - Implement this functions!
+    // this function should return the newly created story so it can be used in the script.js file where it will be appended to the DOM
+    const response = await $.post(`${BASE_URL}/stories`, {
+      token: user.loginToken,
+      story: {
+        author: newStory.author,
+        title: newStory.title,
+        url: newStory.url
+      }
+    });
+
+    const story = new Story(response.story);
+
+    return story;
+  }
+}
+
+
+/**
+ * The User class to primarily represent the current user.
+ *  There are helper methods to signup (create), login, and stayLoggedIn
+ */
+class User {
+  constructor(userObj) {
+    this.username = userObj.username;
+    this.name = userObj.name;
+    this.createdAt = userObj.createdAt;
+    this.updatedAt = userObj.updatedAt;
+
+    // these are all set to defaults, not passed in by the constructor
+    this.loginToken = "";
+    this.favorites = [];
+    this.ownStories = [];
+  }
+
+  /*
+   A class method to create a new user - it accepts a username, password and name
+   It makes a POST request to the API and returns the newly created User as well as a token
+   */
+  static async create(username, password, name) {
+    const response = await $.post(`${BASE_URL}/signup`, {
+      user: {
+        username,
+        password,
+        name
+      }
+    });
+    // build a new User instance from the API response
+    const newUser = new User(response.user);
+
+    // attach the token to the newUser instance for convenience
+    newUser.loginToken = response.token;
+
+    // save the token to localStorage
+    localStorage.setItem("token", response.token);
+
+    // also save the username so that we don't have to decode the token to get it every time
+    localStorage.setItem("username", newUser.username); 
+    return newUser;
+  }
+
+  /*
+   A class method to log in a user. It returns the user 
+   */
+  static async login(username, password) {
+    const response = await $.post(`${BASE_URL}/login`, {
+      user: {
+        username,
+        password
+      }
+    });
+    // build a new User instance from the API response
+    const existingUser = new User(response.user);
+
+    // instantiate Story instances for the user's favorites and ownStories
+    existingUser.favorites = response.user.favorites.map(story => new Story(story))
+    existingUser.ownStories = response.user.stories.map(story => new Story(story));
+
+    // attach the token to the newUser instance for convenience
+    existingUser.loginToken = response.token;
+
+    // save the token to localStorage
+    localStorage.setItem("token", response.token);
+
+    // also save the username so that we don't have to decode the token to get it every time
+    localStorage.setItem("username", existingUser.username);
+
+    return existingUser;
+  }
+
+  /**
+   * This function grabs a token and username from localStorage.
+   *  It uses the token & username to make an API request to get details
+   *   about the user. Then it creates an instance of user with that inf function.
+   */
+  static async stayLoggedIn() {
+    // get username and token from localStorage
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+
+    // call the API
+    const response = await $.getJSON(`${BASE_URL}/users/${username}`, {
+      token
+    });
+    // instantiate the user from the API information
+    const existingUser = new User(response.user);
+
+    // attach the token to the newUser instance for convenience
+    existingUser.loginToken = token;
+
+    // instantiate Story instances for the user's favorites and ownStories
+    existingUser.favorites = response.user.favorites.map(
+      story => new Story(story)
+    );
+    existingUser.ownStories = response.user.stories.map(
+      story => new Story(story)
+    );
+    return existingUser;
+  }
+}
+/**
+ * Class to represent a single story. Has one method to update.
+ */
+class Story {
+  /*
+   * The constructor is designed to take an object for better readability / flexibility
+   */
+  constructor(storyObj) {
+    this.author = storyObj.author;
+    this.title = storyObj.title;
+    this.url = storyObj.url;
+    this.username = storyObj.username;
+    this.storyId = storyObj.storyId;
+    this.createdAt = storyObj.createdAt;
+    this.updatedAt = storyObj.updatedAt;
+  }
+}
---
+++ script.js
@@ -0,0 +1,247 @@
+// global flag to easily tell if we're logged in
+let LOGGED_IN = false;
+
+// global storyList variable
+let storyList;
+
+// global user variable
+let user;
+
+// let's see if we're logged in
+let token = localStorage.getItem("token");
+let username = localStorage.getItem("username");
+
+if (token && username) {
+  LOGGED_IN = true;
+}
+
+$(document).ready(async function() {
+  // cache some selectors we'll be using quite a bit
+  const $allStoriesList = $("#all-articles-list");
+  const $submitForm = $("#submit-form");
+  const $filteredArticles = $("#filtered-articles");
+  const $loginForm = $("#login-form");
+  const $createAccountForm = $("#create-account-form");
+  const $ownStories = $("#my-articles");
+  const $navLogin = $("#nav-login");
+  const $navLogOut = $("#nav-logout");
+  const $navCreate = $("#nav-create");
+  const $createStoryBtn =$("#create-story-submit-button")
+
+  // if there is a token in localStorage, call User.stayLoggedIn
+  //  to get an instance of User with the right details
+  //  this is designed to run once, on page load
+  if (LOGGED_IN) {
+    const userInstance = await User.stayLoggedIn();
+    // we've got a user instance now
+    user = userInstance;
+
+    // let's build out some stories
+    await generateStories();
+
+    // and then display the navigation
+    showNavForLoggedInUser();
+  } else {
+    // we're not logged in, let's just generate stories and stop there
+    await generateStories();
+  }
+
+  /**
+   * Event listener for logging in.
+   *  If successfully we will setup the user instance
+   */
+  $loginForm.on("submit", async function(e) {
+    e.preventDefault(); // no page-refresh on submit
+
+    // grab the username and password
+    const username = $("#login-username").val();
+    const password = $("#login-password").val();
+
+    // call the login static method to build a user instance
+    const userInstance = await User.login(username, password);
+    // set the global user to the user instance
+    user = userInstance;
+    LOGGED_IN = true;
+    loginAndSubmitForm();
+  });
+
+  /**
+   * Event listener for signing up.
+   *  If successfully we will setup a new user instance
+   */
+  $createAccountForm.on("submit", async function(e) {
+    e.preventDefault(); // no page refresh
+
+    // grab the required fields
+    let name = $("#create-account-name").val();
+    let username = $("#create-account-username").val();
+    let password = $("#create-account-password").val();
+
+    // call the create method, which calls the API and then builds a new user instance
+    const newUser = await User.create(username, password, name);
+    user = newUser;
+    loginAndSubmitForm();
+  });
+
+  /**
+   * Log Out Functionality
+   */
+  $navLogOut.on("click", function(e) {
+    // empty out local storage
+    localStorage.clear();
+    // refresh the page, clearing memory
+    location.reload();
+  });
+
+  /**
+   * Event Handler for Clicking Login
+   */
+  $navLogin.on("click", function() {
+    // Show the Login and Create Account Forms
+    $loginForm.slideToggle();
+    $createAccountForm.slideToggle();
+    $allStoriesList.toggle();
+  });
+
+  /**
+   * Event handler for Navigation to Homepage
+   * We save on the number of API requests that we make. This is about 1/3 of the work that needs to be done bc it's only getting the generateStories() function
+   */
+  $("body").on("click", "#nav-all", async function() {
+    hideElements();
+    await generateStories();
+    $allStoriesList.show();
+  });
+
+  /**
+   * A rendering function to run to reset the forms and hide the login info
+   */
+  function loginAndSubmitForm() {
+    // hide the forms for logging in and signing up
+    $loginForm.hide();
+    $createAccountForm.hide();
+
+    // reset those forms
+    $loginForm.trigger("reset");
+    $createAccountForm.trigger("reset");
+
+    // show the stories
+    $allStoriesList.show();
+
+    // update the navigation bar
+    showNavForLoggedInUser();
+  }
+
+  /**
+   * A rendering function to call the StoryList.getStories static method,
+   *  which will generate a storyListInstance.
+   *  Then render it
+   */
+  async function generateStories() {
+    // get an instance of StoryList
+    const storyListInstance = await StoryList.getStories();
+    // update our global variable
+    storyList = storyListInstance;
+    // empty out that part of the page
+    $allStoriesList.empty();
+
+    // loop through all of our stories and generate HTML for them
+    storyList.stories.forEach(function(story) {
+      const result = generateStoryHTML(story);
+      $allStoriesList.append(result);
+    });
+  }
+
+  /**
+   * A function to render HTML for an individual Story instance
+   */
+
+  function generateStoryHTML(story) {
+    let hostName = getHostName(story.url);
+
+    // render story markup
+    const storyMarkup = $(
+      `<li id="${story.storyId}">
+          <a class="article-link" href="${story.url}" target="a_blank">
+            <strong>${story.title}</strong>
+           </a>
+          <small class="article-author">by ${story.author}</small>
+          <small class="article-hostname ${hostName}">(${hostName})</small>
+          <small class="article-username">posted by ${story.username}</small>
+          </li>`
+    );
+
+    return storyMarkup;
+  }
+
+  // hide all elements in elementsArr
+  function hideElements() {
+    const elementsArr = [
+      $submitForm,
+      $allStoriesList,
+      $filteredArticles,
+      $ownStories,
+      $loginForm,
+      $createAccountForm
+    ];
+    elementsArr.forEach(val => val.hide());
+  }
+
+  function showNavForLoggedInUser() {
+    $navLogin.hide();
+    $navLogOut.show();
+    $navCreate.show();
+  }
+
+  // simple function to pull the hostname from a URL
+  function getHostName(url) {
+    let hostName;
+    if (url.indexOf("://") > -1) {
+      hostName = url.split("/")[2];
+    } else {
+      hostName = url.split("/")[0];
+    }
+    if (hostName.slice(0, 4) === "www.") {
+      hostName = hostName.slice(4);
+    }
+    return hostName;
+  }
+
+
+  // Toggle functionality
+  $navCreate.on("click", function() {
+    // Show the create story Forms
+    $submitForm.slideToggle();
+  });
+
+// '#create-story-submit-button'
+
+/** This function is triggered when the form is submitted with new story details.
+ * it calls the API 
+ * gets a response
+ * appends the story to the dom
+ * and hides the form section
+ */
+  $submitForm.on('submit', async function(evt) {
+
+    evt.preventDefault();
+
+    let author = $("#author").val();
+    let title = $("#title").val();
+    let url = $('#url').val();
+    let storyObj = {author, title, url};
+
+    let newStory = await storyList.addStory(user, storyObj);
+
+    $submitForm.slideToggle();
+    appendStory(newStory);
+
+  })
+
+  // Creating a helper function to add a single story to the DOM when a user creates a story so they can see it right away
+    function appendStory(story) {
+      const result = generateStoryHTML(story);
+      $allStoriesList.append(result);
+    }
+
+});
